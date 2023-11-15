33676fa8c3ee32fafa1d5925e31a5fc70a4b98bf
vazha Kozashvili <vakokozashvili@gmail.com>
2023-11-15 21:04:27 +0400

    second commt

--- cookies.js
+++ cookies.js
@@ -24,3 +24,38 @@ cookieMessage = () => {
     document.querySelector("#cookies").style.display = "block";
 };
 window.addEventListener("load", cookieMessage);
+// Function to set analytics cookies
+setAnalyticsCookies = () => {
+  // Replace these values with your actual analytics data
+  const analyticsData = {
+    pagesVisited: ['GB-Renovation', 'about', 'contact'],
+    affiliateReferral: 'example_affiliate',
+    productPurchase: 'example_product',
+  };
+
+  // Convert the data to a JSON string
+  const analyticsJSON = JSON.stringify(analyticsData);
+
+  // Set the analytics cookie with a 30-day expiration
+  setCookies("analytics", analyticsJSON, 30);
+}
+
+// Function to get analytics data from cookies
+getAnalyticsData = () => {
+  const analyticsCookie = getCookie("analytics");
+
+  // If the analytics cookie is present, parse and return the data
+  if (analyticsCookie) {
+    return JSON.parse(analyticsCookie);
+  }
+
+  // Return a default value or handle as needed
+  return {};
+}
+
+// Example usage:
+setAnalyticsCookies(); // Call this when you want to set analytics cookies
+
+// Get analytics data and log it (you can use this data as needed)
+const analyticsData = getAnalyticsData();
+console.log("Analytics Data:", analyticsData);
